8b6b4192bfb82abbc7b8ff01b70f9a1d1d443a59
Bruno Moreno <brunomorello7@gmail.com>
2023-11-02 17:38:24 +0000

    Initialize Repository

---
+++ src/test/java/com/bmo/moviemicroservice/MovieMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmo.moviemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
